a492d6078828f9788c355514f3779b26dc94c4fe
gonnajoong <gonnajoong@gmail.com>
2021-04-26 14:34:10 +0900

    프로젝트 영역 스타일 수정

--- resources/views/index/project.blade.php
+++ resources/views/index/project.blade.php
@@ -6,6 +6,10 @@
                 <span>
                     <img src="#" alt="프로젝트 이미지, 아이콘"/>
                     <p>{{$projects['project_name']}}</p>
+                    <span>담당 기간: {{$projects['contribution']}}</span>
+                    <span>작성언어: {{$projects['making_lang']}}</span>
+                    <span>서비스: {{$projects['is_operation']}}</span>
+                    <span>담당 역할: {{$projects['role']}}</span>
                 </span>
             </li>
         @endforeach
