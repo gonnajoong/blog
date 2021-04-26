1d164124959807ce903cb8f4826ffff3627e598f
gonnajoong <gonnajoong@gmail.com>
2021-04-26 14:17:38 +0900

    폰트 추가

--- resources/views/index/footer.blade.php
+++ resources/views/index/footer.blade.php
@@ -1,7 +1,6 @@
 <footer class="gj-footer-wrap">
     <address>
-        <span>Email: <a href="mailto:gonnajoong@gmail.com">gonnajoong@gmail.com</a></span><br/>
-        <span>Phone: <a href="tel:01032906667">010-3290-6667</a></span><br/>
+        <span>Email: <a href="mailto:gonnajoong@gmail.com">gonnajoong@gmail.com</a></span>
         <span>Copyright ⓒ GonnaJoong. 2021 All Rights Reserved</span>
     </address>
 </footer>
